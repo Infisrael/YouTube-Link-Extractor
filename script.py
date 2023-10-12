d2670ca13977060cde1455e4f9cbc7e45dab75c8
Infisrael <tieo9435@gmail.com>
2023-10-12 07:20:14 +0300

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ import re
 # Define a regular expression pattern to match YouTube video URLs
 pattern = r'https://www\.youtube\.com/watch\?v=([a-zA-Z0-9_-]+)'
 
+# Initialize a set to store unique video IDs
+unique_video_ids = set()
+
 # Open and read the text file, replace file.txt with your file you want to extract the YouTube links from
 with open('file.txt', 'r') as file:
     text = file.read()
